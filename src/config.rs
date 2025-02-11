d21de9b736287d5f334e0f1b85280689a44a4380
breaktely <zhaotianrui_00@163.com>
2025-02-12 00:40:05 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -97,8 +97,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["sdp.trzhao.store"];
+pub const RS_PUB_KEY: &str = "za6NdOf+gyFh9VXwf9k7Ez9Yxj7a9gEJlihA73IE0UI=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
